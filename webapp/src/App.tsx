aff4b7e24166d206621510a886e635596825852f
Max Wu <mxwu1994@gmail.com>
2021-01-24 04:52:26 +1100

    precompile steps to include merging extend types into just one type to make schema.graphql compatible with relay compiler

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { graphql } from 'babel-plugin-relay/macro'
+import {QueryRenderer} from "react-relay";
+import environment from "./relayEnvironment";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryRenderer
+      environment={environment}
+      query={graphql`
+          query AppQuery {
+            users {
+              pageInfo {
+                hasNextPage
+                hasPreviousPage
+                startCursor
+                endCursor
+              }
+              edges {
+                cursor
+                node {
+                  name
+                  email
+                }
+              }
+            }
+          }
+        `}
+      variables={{}}
+      render={({error, props}) => {
+        if (error) {
+          return <div>Error!</div>;
+        }
+        if (!props) {
+          return <div>Loading...</div>;
+        }
+        return <div>Query result: {JSON.stringify(props)}</div>;
+      }}
+    />
   );
 }
 
---
+++ webapp/src/relayEnvironment.ts
@@ -0,0 +1,34 @@
+import {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} from 'relay-runtime';
+
+import {RequestParameters} from 'relay-runtime/lib/util/RelayConcreteNode';
+import {Variables} from 'relay-runtime/lib/util/RelayRuntimeTypes';
+
+function fetchQuery(
+  operation: RequestParameters,
+  variables: Variables,
+) {
+  return fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json();
+  });
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+export default environment;
